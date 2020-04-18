8cbe30ab63da22e856a2f6e8a9e21fa22d1eac36
mgoin <michael@neuralmagic.com>
2020-04-18 17:07:17 -0400

    We have a glider!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,93 @@
 use std::fmt;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 enum CellState {
-    Alive,
     Dead,
+    Alive,
 }
 
+#[derive(Clone)]
 struct Grid {
     width: usize,
     height: usize,
+    step: usize,
     data: Vec<CellState>,
+}
+
+impl Grid {
+    fn new(width: usize, height: usize) -> Grid {
+        Grid {
+            width,
+            height,
+            step: 0,
+            data: vec![CellState::Dead; width * height],
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> Option<CellState> {
+        if x < self.width && y < self.height {
+            Some(self.data[y * self.width + x])
+        } else {
+            None
+        }
+    }
+
+    fn set(&mut self, x: usize, y: usize, state: CellState) {
+        self.data[y * self.width + x] = state;
+    }
+
+    fn neighbors(&self, x: usize, y: usize) -> Vec<(CellState, usize, usize)> {
+        let mut v: Vec<(CellState, usize, usize)> = Vec::new();
+        for i in -1i32..=1 {
+            for j in -1i32..=1 {
+                if i != 0 || j != 0 {
+                    let new_x = (x as i32 + j) as usize;
+                    let new_y = (y as i32 + i) as usize;
+                    if let Some(cs) = self.get(new_x, new_y) {
+                        v.push((cs, new_x, new_y));
+                    }
+                }
+            }
+        }
+        v
+    }
+
+    fn update(&mut self) {
+        // Make a copy of current grid to use for rules
+        let g = self.clone();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let is_alive = match g.get(x, y) {
+                    Some(cs) => cs == CellState::Alive,
+                    None => false,
+                };
+
+                let alive_neighbors = g
+                    .neighbors(x, y)
+                    .iter()
+                    .filter(|&n| n.0 == CellState::Alive)
+                    .count();
+
+                match (is_alive, alive_neighbors) {
+                    // Birth: Any dead cell with three live neighbors becomes a live cell
+                    (false, 3) => self.set(x, y, CellState::Alive),
+                    // Survival: Any live cell with two or three live neighbors survives
+                    (true, 2..=3) => self.set(x, y, CellState::Alive),
+                    // All other live cells die, all other dead cells stay dead
+                    (true, _) => self.set(x, y, CellState::Dead),
+                    _ => {}
+                }
+            }
+        }
+        self.step += 1;
+    }
 }
 
 impl fmt::Display for CellState {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            CellState::Alive => write!(f, "1"),
-            CellState::Dead => write!(f, "0"),
+            CellState::Dead => write!(f, "."),
+            CellState::Alive => write!(f, "*"),
         }
     }
 }
@@ -25,7 +96,10 @@ impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for y in 0..self.height {
             for x in 0..self.width {
-                write!(f, "{} ", self.get(x, y))?;
+                match self.get(x, y) {
+                    Some(cs) => write!(f, "{} ", cs)?,
+                    None => write!(f, "~ ")?,
+                }
             }
             writeln!(f)?;
         }
@@ -33,27 +107,17 @@ impl fmt::Display for Grid {
     }
 }
 
-impl Grid {
-    fn new(width: usize, height: usize) -> Grid {
-        Grid {
-            width,
-            height,
-            data: vec![CellState::Dead; width * height],
-        }
-    }
-
-    fn get(&self, x: usize, y: usize) -> CellState {
-        self.data[y * self.width + x]
-    }
-
-    fn set(&mut self, x: usize, y: usize, state: CellState) {
-        self.data[y * self.width + x] = state;
-    }
-
-    // fn update(&mut self, rules: impl Fn())
-}
-
 fn main() {
-    let grid = Grid::new(10, 10);
+    let mut grid = Grid::new(5, 5);
+    grid.set(0, 1, CellState::Alive);
+    grid.set(1, 2, CellState::Alive);
+    grid.set(2, 0, CellState::Alive);
+    grid.set(2, 1, CellState::Alive);
+    grid.set(2, 2, CellState::Alive);
     println!("{}", grid);
+
+    for _ in 0..6 {
+        grid.update();
+        println!("{}", grid);
+    }
 }
